73a741326187aab4dd1ec96e180513f3fac74c82
Douglas Lopes <120102477+sieg3d@users.noreply.github.com>
2023-03-01 23:01:06 +0000

    update

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ opcao = prompt('Selecione a questão:\n\t1\n\t2\n\t3\n\t4\n\t5\n\t6\n\t7\n\t8\n\
             document.write(`1. Faça um programa que sorteia um número de 0 a 9999 e mostre na tela cada um dos dígitos separadamente. <br>Exemplo: unidade: 4 dezena: 3 centena: 8 milhar`)
 
             sorteio = Math.random() * 10000
-            unidade = sorteio % 10;
-            dezena = Math.floor((sorteio / 10) % 10);
-            centena = Math.floor((sorteio / 100) % 10);
-            milhar = Math.floor(sorteio / 1000);
+            unidade = Math.floor(sorteio % 10)
+            dezena = Math.floor((sorteio / 10) % 10)
+            centena = Math.floor((sorteio / 100) % 10)
+            milhar = Math.floor(sorteio / 1000)
 
             document.write(`<p>O número sorteado foi: ${sorteio.toFixed(0)}</p>`)
 
-            document.write(`Unidade: ${unidade.toFixed(0)}| Dezena: ${dezena.toFixed(0)}| Centena: ${centena.toFixed(0)}| Milhar: ${milhar.toFixed(0)}`)
+            document.write(`Unidade: ${unidade}| Dezena: ${dezena}| Centena: ${centena}| Milhar: ${milhar}`)
 
       
             break
