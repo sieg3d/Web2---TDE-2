97294c1bac0a8a41d839c5ff215eb23275b068c0
Douglas Lopes <120102477+sieg3d@users.noreply.github.com>
2023-03-01 02:20:45 +0000

    1 concluída. Inicio da 2.

---
+++ script.js
@@ -0,0 +1,14 @@
+/*document.write(`1. Faça um programa que sorteia um número de 0 a 9999 e mostre na tela cada um dos dígitos separadamente. <br>Exemplo: unidade: 4 dezena: 3 centena: 8 milhar`)
+
+sorteio=Math.random()*10000
+unidade = sorteio % 10;
+dezena = Math.floor((sorteio / 10) % 10);
+centena = Math.floor((sorteio / 100) % 10);
+milhar = Math.floor(sorteio / 1000);
+
+document.write(`<p>O número sorteado foi: ${sorteio.toFixed(0)}</p>`)
+
+document.write(`Unidade: ${unidade.toFixed(0)}| Dezena: ${dezena.toFixed(0)}| Centena: ${centena.toFixed(0)}| Milhar: ${milhar.toFixed(0)}`)
+*/
+
+
